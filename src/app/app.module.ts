f315180bda228da335c566d264e7e7c60e3e27df
ADHARMAN\sdharavath <shiva.dharavath@harman.com>
2018-02-19 17:11:26 +0530

    Added folders test files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { HeaderComponent } from './shared/header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
+import { TestDirective } from './directives/test.directive';
+import { TestPipe } from './pipes/test.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    TestDirective,
+    TestPipe,
   ],
   imports: [
     BrowserModule,
